ea94259ced74fc4de3b27835f8d91110a72dc541
W1nter998 <134254764+W1nter998@users.noreply.github.com>
2025-05-23 14:32:50 +0800

    Add files via upload

---
+++ supabase.js
@@ -0,0 +1,7 @@
+
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
+
+export const supabase = createClient(
+  'https://eiaxcwfyocnnwhkdgioh.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVpYXhjd2Z5b2Nubndoa2RnaW9oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc5NzYyNjEsImV4cCI6MjA2MzU1MjI2MX0.k2SfqrA7mFieCPePznOt5joGO03b7c1DlaH5tjtYq_E'
+);
