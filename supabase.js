fddbcbf565ea64039a0b9cd04a782fb36908bf10
W1nter998 <134254764+W1nter998@users.noreply.github.com>
2025-05-23 16:39:11 +0800

    Add files via upload

--- supabase.js
+++ supabase.js
@@ -1,12 +1,8 @@
 // supabase.js
-import { createClient as _createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
-export const createClient = (url, key) => {
-  return _createClient(url, key);
-};
+// Supabase 项目地址和匿名 key
+const SUPABASE_URL = 'https://eiaxcwfyocnnwhkdgioh.supabase.co'
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVpYXhjd2Z5b2NubndoaW9oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc5NzYyNjEsImV4cCI6MjA2MzU1MjI2MX0.k2SfqrA7mFieCPePznOt5joGO03b7c1DlaH5tjtYq_E'
 
-// 全局实例
-export const supabase = createClient(
-  'https://eiaxcwfyocnnwhkdgioh.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVpYXhjd2Z5b2NubndoaW9oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc5NzYyNjEsImV4cCI6MjA2MzU1MjI2MX0.k2SfqrA7mFieCPePznOt5joGO03b7c1DlaH5tjtYq_E'
-);
+export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
